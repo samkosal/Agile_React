96e8c489fa2360cb7753b2c801aebc50ffb48f63
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-21 11:55:11 -0700

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import data from './data.json';
-import header from './Header/Header'
-import ArtistList from './ArtistList/ArtistList'
+import Header from './Header/Header';
+// import ArtistList from './ArtistList/ArtistList';
 
 export default function App() {
   return (
     <>
-      <header />
-      <ArtistList />
+      <Header />
+      {/* <ArtistList /> */}
     </>
-  )
+  );
 }
