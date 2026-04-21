584e4943c0f0d28cb3f99b5021c048332fb67b56
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-21 11:43:43 -0700

    App should render label Header, then ArtistList

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import data from './data.json';
+import header from './Header/Header'
+import ArtistList from './ArtistList/ArtistList'
+
+export default function App() {
+  return (
+    <>
+      <header />
+      <ArtistList />
+    </>
+  )
+}
