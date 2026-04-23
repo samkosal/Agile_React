908ca3cef375af6a65abf0f6deadc625df800286
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-23 00:10:42 -0700

    finished the header jsx and css

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,15 @@
+import './Header.css';
+
 export default function Header() {
     return (
-        <header>
-            <img src="src/assets/Record_Label.jpg"/>
-            <h1> my travel journal</h1>
+        <header className="header-container">
+            <div className="header-logo-title">
+                <img className="header-logo" src="src/assets/Record_Label.jpg" alt="Wavelength Records Logo" />
+                <div>
+                    <h1 className="header-title">Wavelength Records</h1>
+                    <h2 className="header-subtitle">Independent Label &bull; Seattle, WA</h2>
+                </div>
+            </div>
         </header>
-    )
+    );
 }
